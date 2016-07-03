150c99a58beefe7174ed9c31cebd2b0aec387223
daynyellcat <dkespra@gmail.com>
2016-07-03 21:34:29 +0800

    Mobile App Progress

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+         
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('profilePageCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('listOfCarsCtrl', function($scope) {
+
+})
+   
+.controller('availableCarForRentingCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('myRentalRecordCtrl', function($scope) {
+
+})
+ 
